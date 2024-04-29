d84677d96658c3f9751e51b6f12ab17945d4b6c8
kavya <kavyapothuguntla23@gmail.com>
2024-04-29 17:59:37 -0400

    functions

--- common.sh
+++ common.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
@@ -9,8 +8,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-echo "Please enter DB password:"
-read -s mysql_root_password
+
 
 VALIDATE(){
     if [ $1 -ne 0 ]
