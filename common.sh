34408c1bbc2bf7bbff462a889544122b83aea670
kavya <kavyapothuguntla23@gmail.com>
2024-04-29 17:56:19 -0400

    functions

--- common.sh
+++ common.sh
@@ -23,10 +23,10 @@ VALIDATE(){
 }
 
 check_root(){
-if [ $USERID -ne 0 ]
-then
-    echo "please run this script with root access."
-    exit 1 # manually exit if error comes.
+    if [ $USERID -ne 0 ]
+    then
+        echo "please run this script with root access."
+        exit 1 # manually exit if error comes.
     else
         echo "you are super user."
     fi
