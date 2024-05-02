479d177b2198944ba27c561fe68ebeba7744e67f
kavya <kavyapothuguntla23@gmail.com>
2024-05-02 17:59:56 -0400

    functions

--- mysql.sh
+++ mysql.sh
@@ -8,23 +8,23 @@ echo "Please enter DB password:"
 read -s mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
-# VALIDATE $? "Installing MYSQL Server"
+VALIDATE $? "Installing MYSQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
-# VALIDATE $? "Enabling MYSQL Server"
+VALIDATE $? "Enabling MYSQL Server"
 
 systemctl start mysqld &>>$LOGFILE
-# VALIDATE $? "Starting MySQL Server"
+VALIDATE $? "Starting MySQL Server"
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# VALIDATE $? "Setting up root password"
+VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
  mysql -h db.kavya.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
  if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    # VALIDATE $? "MYSQL Root password setup"
+    VALIDATE $? "MYSQL Root password setup"
 else
     echo -e "MYSQL Root password is already setup...$Y SKIPPING $N"
 fi
