6f7f1b6a030f627def84cabbc153abdf4f9d3e8c
kavya <kavyapothuguntla23@gmail.com>
2024-05-02 18:01:19 -0400

    functions

--- mysql.sh
+++ mysql.sh
@@ -21,7 +21,7 @@ VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
  mysql -h db.kavya.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
- if [ $? -ne 0 ]
+if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "MYSQL Root password setup"
