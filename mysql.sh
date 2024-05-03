0131e42f910ed8114aa5b114a1421a473e400258
kavya <kavyapothuguntla23@gmail.com>
2024-05-03 12:16:28 -0400

    functions

--- mysql.sh
+++ mysql.sh
@@ -14,7 +14,7 @@ systemctl enable mysqld &>>$LOGFILE
 systemctl start mysqld &>>$LOGFILE
 
 #Below code will be useful for idempotent nature
- mysql -h db.kavya.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.kavya.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
